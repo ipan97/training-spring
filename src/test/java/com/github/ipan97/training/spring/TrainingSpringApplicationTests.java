72c20a069cf6e7c30097535dcd4367dd77c5d00d
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2015-12-17 17:38:38 +0700

    start training spring

---
+++ src/test/java/com/github/ipan97/training/spring/TrainingSpringApplicationTests.java
@@ -0,0 +1,18 @@
+package com.github.ipan97.training.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TrainingSpringApplication.class)
+@WebAppConfiguration
+public class TrainingSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
